fd2ebf52798099ec91f93165fd9a7e931df37734
sociallabs <siddarthpaim@gmail.com>
2024-04-13 19:35:21 -0400

    Java Project

---
+++ stock-analysis-group/src/main/java/com/siddarth/pai/stockanalysisgroup/StockAnalysisGroupApplication.java
@@ -0,0 +1,13 @@
+package com.siddarth.pai.stockanalysisgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalysisGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalysisGroupApplication.class, args);
+	}
+
+}
---
+++ stock-analysis-group/src/test/java/com/siddarth/pai/stockanalysisgroup/StockAnalysisGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddarth.pai.stockanalysisgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalysisGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
